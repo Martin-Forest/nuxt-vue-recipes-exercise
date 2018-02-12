e0c091d2bf82bb2def8da2c683862203723c2c81
Martin Forest <martin.forest@blueskytp.com>
2018-02-11 20:00:48 -0500

    Add recipe detail  page.  Add navigation header. Add ayncData function.

--- pages/recipes/_id/index.vue
+++ pages/recipes/_id/index.vue
@@ -1,52 +1,53 @@
 <template>
-    <section class="recipes">
-        <Recipe
-            v-for="recipe in recipes"
-            :key="recipe.id"
-            :thumbnail="recipe.thumbnail"
-            :title="recipe.title"
-            :id="recipe.id"
-            :previewText="recipe.previewText"/>    
+    <section class="single-recipe">
+        <h1>{{recipe.title}}</h1>
+        <div>
+            <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title">
+        </div>
+        <p>{{recipe.descriptionText}}</p>
     </section>
 </template>
 
 <script>
-    import Recipe from '@/components/Recipe'
-
     export default {
-        components: {
-            Recipe
-        },
-        asyncData() {
+       asyncData(context) {
             return new Promise((resolve,reject) => {
                 setTimeout(() => {
                     resolve({
-                        recipes: [
+                        recipe: [
                             {
                                 id: "1",
                                 title: "Delicious chicken",
-                                previewText: "Awesome pressure-cooked chicken!!!",
+                                previewtext: "Awesome pressure-cooked chicken!!!",
+                                descriptionText: "These are the steps for the detailed recipe.",
                                 thumbnail: "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg"
                             },
                            {
                                 id: "2",
                                 title: "Awesome beef stew",
-                                previewText: "Delicious pressure-cooked beef stew. Yummy!!!",                                
+                                previewtext: "Delicious pressure-cooked beef stew. Yummy!!!",
+                                descriptionText: "These are the instructions for the yummy recipe to warm your heart.",                                
                                 thumbnail: "http://worldwidetrophyadventures.com/blog/wp-content/uploads/2016/10/earls-stew.jpg"
                             }                            
-                        ]
+                        ].find(el => el.id === context.params.id)
                     })
                 },1500)
             })
-        }
+        }       
     }
 </script>
 
 <style scoped>
-.recipes {
+.single-recipe {
     display: flex;
-    flex-flow: row wrap;
+    flex-flow: column;
     justify-content: center;
     align-items: center;
+    text-align: center;
+    padding: 30px;
+}
+
+.recipe-image {
+    width: 100%;
 }
 </style>
